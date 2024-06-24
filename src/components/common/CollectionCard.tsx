3d70669d5b0c3a9994beb19ac885d3a9e06fe998
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-06-24 17:31:48 +0300

    Array.slice in CollectionCard

--- src/components/common/CollectionCard.tsx
+++ src/components/common/CollectionCard.tsx
@@ -19,19 +19,21 @@ export interface CollectionCardProps {
 const CollectionCard: FC<CollectionCardProps> = ({ collection }) => (
   <figure className="flex flex-col gap-3.5">
     <div className="grid grid-cols-3 gap-3.5 first:*:col-span-3">
-      {[collection.previews[0], collection.previews[1], collection.previews[2]].map(
-        (preview: string | undefined, index) =>
-          preview ? (
-            <img
-              key={index}
-              src={preview}
-              alt=""
-              className="aspect-square w-full rounded-[1.25rem] object-cover transition-transform duration-300 [:hover>*>&]:first:-translate-y-4 [:hover>*>&]:first:scale-110"
-            />
-          ) : (
-            <PreviewPlaceholder key={index} />
-          ),
-      )}
+      {collection.previews.slice(0, 3).map((preview, index) => (
+        <img
+          key={index}
+          src={preview}
+          alt=""
+          className="aspect-square w-full rounded-[1.25rem] object-cover transition-transform duration-300 [:hover>*>&]:first:-translate-y-4 [:hover>*>&]:first:scale-110"
+        />
+      ))}
+
+      {Array(3 - collection.previews.length)
+        .fill(undefined)
+        .map((_, index) => (
+          <PreviewPlaceholder key={index} />
+        ))}
+
       <div className="flex items-center justify-center rounded-[1.25rem] bg-accent font-mono text-2xl">
         <p>{collection.size - 3}+</p>
       </div>
